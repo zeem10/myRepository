05ac1fea8c391a24623a7a53c3cc3a899ccc9b18
admin604 <admin604@DESKTOP-76M8ALU>
2018-11-24 07:27:07 +0700

    Commit proyek pertama

---
+++ src/main/java/org/if_unpas/kapl/ContohVersioning/App.java
@@ -0,0 +1,13 @@
+package org.if_unpas.kapl.ContohVersioning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
