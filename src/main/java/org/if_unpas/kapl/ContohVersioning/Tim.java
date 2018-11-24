aab0aebd1604f4412b4cd89e71803e56d00c909d
admin604 <admin604@DESKTOP-S8903A2>
2018-11-24 08:14:51 +0700

    tim B kedua

--- src/main/java/org/if_unpas/kapl/ContohVersioning/Tim.java
+++ src/main/java/org/if_unpas/kapl/ContohVersioning/Tim.java
@@ -6,12 +6,29 @@ import java.util.List;
 public class Tim {
 	private String nama;
 	private List<Pemain> pemainInti = new ArrayList<Pemain>();
-	
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public List<Pemain> getPemainInti() {
+		return pemainInti;
+	}
+
+	public void setPemainInti(List<Pemain> pemainInti) {
+		this.pemainInti = pemainInti;
+	}
+
 	public Tim() {
 		nama = "";
 	}
-	
+
 	public Tim(String nama) {
 		this.nama = nama;
 	}
+
 }
