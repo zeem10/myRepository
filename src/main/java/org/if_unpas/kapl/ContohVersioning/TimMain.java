a1d3c3fd1c724e4fd4edb658e3d0156d3169a7ce
admin604 <admin604@DESKTOP-76M8ALU>
2018-11-24 08:26:04 +0700

    Beres BOSS!!!

--- src/main/java/org/if_unpas/kapl/ContohVersioning/TimMain.java
+++ src/main/java/org/if_unpas/kapl/ContohVersioning/TimMain.java
@@ -1,7 +1,18 @@
 package org.if_unpas.kapl.ContohVersioning;
 
 public class TimMain {
-	public static void main(String[]args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) {
+		Tim tim = new Tim("Persib");
+		
+		Pemain pemain1 = new Pemain("I Made", 1);
+		Pemain pemain2 = new Pemain("Atep", 7);
+		Pemain pemain3 = new Pemain("Essien", 5);
+		
+		tim.tambahPemainInti(pemain1);
+		tim.tambahPemainInti(pemain2);
+		tim.tambahPemainInti(pemain3);
+		tim.tambahPemainInti(new Pemain("Febry", 22));
+		
+		tim.tampilPemainInti();
 	}
 }
